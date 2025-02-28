8dfc354f6bdcb663129c7c8f57ceefa6618860e1
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2025-02-28 11:41:29 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class Student{
+public int id;
+public String name;
+public static void main(String args[]){
+Student S1=new Student();
+S1.id=101;
+S1.name="Manan";
+System.out.println(S1.id);
+System.out.println(S1.name);
+
+}
+}
